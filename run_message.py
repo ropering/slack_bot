7822ee1399ab4792b964990d1b40046564f955db
Myeong Gyun O <bonoboss1028@gmail.com>
2022-08-28 21:14:51 +0900

    token 변수 상수 컨벤션으로 변경

--- run_message.py
+++ run_message.py
@@ -1,7 +1,8 @@
 from slack_sdk import WebClient
-from settings import token
+from settings import TOKEN
 
-client = WebClient(token=token)
+client = WebClient(token=TOKEN)
 
 client.chat_postMessage(channel="public",
                         text="안녕하세요 봇입니다~")
+
